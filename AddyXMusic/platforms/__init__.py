27526ad714929bd7d9bfcee9e704b706390a8aad
AdarshuXD <108065687+AdarshuXD@users.noreply.github.com>
2023-03-30 05:54:45 +0530

    Add files via upload

--- AddyXMusic/platforms/__init__.py
+++ AddyXMusic/platforms/__init__.py
@@ -1,1 +1,18 @@
+#
+# Copyright (C) 2023-2024 by AdarshuXD@Github, < https://github.com/AdarshuXD >.
+# A Powerful Music Bot Property Of Unknown To Friends Indian Largest Chatting Group
 
+# Kanged By © @SexyCoder
+# U2F © @Unknown2Friends
+# Owner Addy
+# Sexy Coder 
+# All rights reserved. © Addy © AD © Coder
+
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
