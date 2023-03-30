603daa5c67b1367daa15c7ecb0bd42142a5921df
AdarshuXD <108065687+AdarshuXD@users.noreply.github.com>
2023-03-30 05:49:28 +0530

    Add files via upload

--- AddyXMusic/plugins/modules/id.py
+++ AddyXMusic/plugins/modules/id.py
@@ -1,1 +1,15 @@
+from AddyXMusic import app
+from pyrogram import filters
 
+
+@app.on_message(filters.command("id"))
+def ids(_, message):
+    reply = message.reply_to_message
+    if reply:
+        message.reply_text(
+            f"**Your ID**: `{message.from_user.id}`\n**{reply.from_user.first_name}'s ID**: `{reply.from_user.id}`\n**Chat ID**: `{message.chat.id}`"
+        )
+    else:
+        message.reply(
+            f"**Your ID**: `{message.from_user.id}`\n**Chat ID**: `{message.chat.id}`"
+        )
